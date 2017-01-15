837fd87029c29f40b63120de4df2336ce2902004
AGWard <anthony.ward.data@gmail.com>
2017-01-15 13:14:47 -0400

    Deleted MainStoryBoard & ViewController. Created HomeController as root view. Also changed the loading screen to white to ensure this view is being loaded

--- ShowMe/HomeController.swift
+++ ShowMe/HomeController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeController.swift
 //  ShowMe
 //
 //  Created by Anthony Ward on 1/15/17.
@@ -8,18 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class HomeController: UIViewController {
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = .white
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
